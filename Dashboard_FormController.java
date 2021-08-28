e7a90469103f9cab3dd35a18f465a2c2124627fb
Lovish Jindal <88631544+Lovish0181@users.noreply.github.com>
2021-08-28 23:10:07 +0530

    Add files via upload

---
+++ Dashboard_FormController.java
@@ -0,0 +1,169 @@
+package Dashboard;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class Dashboard_FormController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView imglogout;
+
+    @FXML
+    private Button btndeveloper;
+
+
+    @FXML
+    void doavail(MouseEvent event) throws IOException {
+    	FXMLLoader fxmlloader= new FXMLLoader(getClass().getResource("/PropertySales/PropertySales_Form.fxml"));
+    	Parent root=(Parent)fxmlloader.load();
+    	
+    	Stage stage=new Stage();
+    	stage.setScene(new Scene(root));
+    	stage.show();
+
+    }
+
+    @FXML
+    void docregistration(MouseEvent event) throws IOException {
+    	FXMLLoader fxmlloader= new FXMLLoader(getClass().getResource("/CustomerRegistration/CustomerRegistration_Form.fxml"));
+    	Parent root=(Parent)fxmlloader.load();
+    	
+    	Stage stage=new Stage();
+    	stage.setScene(new Scene(root));
+    	stage.show();
+
+    }
+
+    @FXML
+    void docview(ActionEvent event) throws IOException {
+    	FXMLLoader fxmlloader= new FXMLLoader(getClass().getResource("/CustomerView/CustomerView_Form.fxml"));
+    	Parent root=(Parent)fxmlloader.load();
+    	
+    	Stage stage=new Stage();
+    	stage.setScene(new Scene(root));
+    	stage.show();
+
+    }
+
+    @FXML
+    void dodeals(MouseEvent event) throws IOException {
+    	FXMLLoader fxmlloader= new FXMLLoader(getClass().getResource("/DealsMatured/DealsMatured_Form.fxml"));
+    	Parent root=(Parent)fxmlloader.load();
+    	
+    	Stage stage=new Stage();
+    	stage.setScene(new Scene(root));
+    	stage.show();
+
+    }
+
+    @FXML
+    void dodemand(MouseEvent event) throws IOException {
+    	FXMLLoader fxmlloader= new FXMLLoader(getClass().getResource("/CustomerDemand/CustomerDemand_Form.fxml"));
+    	Parent root=(Parent)fxmlloader.load();
+    	
+    	Stage stage=new Stage();
+    	stage.setScene(new Scene(root));
+    	stage.show();
+
+    }
+
+    @FXML
+    void dodeveloper(ActionEvent event) {
+    	try {
+    	FXMLLoader fxmlloader= new FXMLLoader(getClass().getResource("/Developer/Developer_Form.fxml"));
+    	Parent root=(Parent)fxmlloader.load();
+    	
+    	Stage stage=new Stage();
+    	stage.setScene(new Scene(root));
+    	stage.show();
+		 Scene scene1=(Scene)btndeveloper.getScene();
+		   scene1.getWindow().hide();
+	}
+	catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+
+    }
+
+    @FXML
+    void doimglogout(MouseEvent event) {
+    	try{
+    		FXMLLoader fxmlloader= new FXMLLoader(getClass().getResource("/Login/Login_Form.fxml"));
+        	Parent root=(Parent)fxmlloader.load();
+        	
+        	Stage stage=new Stage();
+        	stage.setScene(new Scene(root));
+        	stage.show();
+            
+        	Scene scene1=(Scene)imglogout.getScene();
+			scene1.getWindow().hide();
+
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+    }
+
+    @FXML
+    void dopic(ActionEvent event) throws IOException {
+    	FXMLLoader fxmlloader= new FXMLLoader(getClass().getResource("/PicGallery/PicGallery_Form.fxml"));
+    	Parent root=(Parent)fxmlloader.load();
+    	
+    	Stage stage=new Stage();
+    	stage.setScene(new Scene(root));
+    	stage.show();
+
+    }
+
+    @FXML
+    void doproperty(ActionEvent event) throws IOException {
+    	FXMLLoader fxmlloader= new FXMLLoader(getClass().getResource("/FindProperty/FindProperty_Form.fxml"));
+    	Parent root=(Parent)fxmlloader.load();
+    	
+    	Stage stage=new Stage();
+    	stage.setScene(new Scene(root));
+    	stage.show();
+
+    }
+
+    @FXML
+    void doregistered(ActionEvent event) {
+    	try {
+    		FXMLLoader fxmlloader= new FXMLLoader(getClass().getResource("/FindProperty1/FindProperty1_Form.fxml"));
+        	Parent root;
+    		root = (Parent)fxmlloader.load();
+			Stage stage=new Stage();
+	    	stage.setScene(new Scene(root));
+	    	stage.show();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+    }
+
+    @FXML
+    void initialize() {
+            }
+}
--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package FindProperty1;
+package Dashboard;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,8 +12,8 @@ public class Main extends Application
  public void start(@SuppressWarnings("exports") Stage primaryStage) 
    {
 		try {
-				Parent root=(Parent) FXMLLoader.load(getClass().getResource("FindProperty1_Form.fxml")); 
-				Scene scene = new Scene(root,800,700);
+				Parent root=(Parent) FXMLLoader.load(getClass().getResource("Dashboard_Form.fxml")); 
+				Scene scene = new Scene(root,1000,700);
 				//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 				primaryStage.setScene(scene);
 				primaryStage.show();
