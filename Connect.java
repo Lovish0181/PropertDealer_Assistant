299005c8bcec225d6596e6ebde19b263d80184cd
Lovish Jindal <88631544+Lovish0181@users.noreply.github.com>
2021-09-15 02:51:01 +0530

    Add files via upload

--- Connect.java
+++ Connect.java
@@ -1,4 +1,4 @@
-package FindProperty;
+package ForgotPassword;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Connection;
--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package FindProperty;
+package ForgotPassword;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,8 +12,8 @@ public class Main extends Application
  public void start(@SuppressWarnings("exports") Stage primaryStage) 
    {
 		try {
-				Parent root=(Parent) FXMLLoader.load(getClass().getResource("FindProperty_Form.fxml")); 
-				Scene scene = new Scene(root,800,700);
+				Parent root=(Parent) FXMLLoader.load(getClass().getResource("Forgotpass_Form.fxml")); 
+				Scene scene = new Scene(root,450,500);
 				//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 				primaryStage.setScene(scene);
 				primaryStage.show();
